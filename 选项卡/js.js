c4fccf0746b4b4c782238aa9a72b63ef192d4f9a
kaola <1921346727@qq.com>
2019-04-13 21:04:08 +0800

    一个简单的选项卡

---
+++ 选项卡/js.js
@@ -0,0 +1,99 @@
+window.onload = function(){
+	addpro();
+	initial();
+	allchoice();
+	out();
+}
+
+
+var tr = document.getElementsByTagName('tr');
+var table = document.getElementsByTagName('table')[0];
+var all = document.getElementById('all');
+var nall = document.getElementById('nall');
+var delet = document.getElementById('delet');
+var add = document.getElementById('add');
+var back = document.getElementById('back');
+
+function addtr()  //添加选项
+{
+	var newtr = document.createElement('tr');
+	var newtd_1 = document.createElement('td');
+	var newtd_2 = document.createElement('td');
+	newtd_1.setAttribute('class','left');
+	newtd_2.setAttribute('class','right');
+	var newcheckbox = document.createElement('input');
+	newcheckbox.setAttribute('type','checkbox');
+	newcheckbox.setAttribute('class','checkbox');
+	var newtext_1 = document.createElement('input');
+	newtext_1.setAttribute('type','text');
+	var newtext_2 = document.createElement('input');
+	newtext_2.setAttribute('type','text');
+	newtd_1.appendChild(newcheckbox);
+	newtd_1.appendChild(newtext_1);
+	newtd_2.appendChild(newtext_2);
+	newtr.appendChild(newtd_1);
+	newtr.appendChild(newtd_2);
+	table.appendChild(newtr);
+}
+
+function addpro() //添加选项
+{
+	add.onclick = function()
+	{
+		addtr();
+	}
+}
+
+function origin(target, firstsentence, secondsentence) //初始化选项
+{
+	var td_first = target.getElementsByTagName('td')[0].getElementsByTagName('input')[1];
+	var td_second = target.getElementsByTagName('td')[1].getElementsByTagName('input')[0];
+	td_first.value = firstsentence;
+	td_second.value = secondsentence;
+}
+
+function initial()  //初始化选项卡
+{
+	origin(tr[0],'成全','刘若英');
+	origin(tr[1],'我管你','华晨宇');
+	origin(tr[2],'走马','陈粒');
+	origin(tr[3],'房间','刘瑞琦');
+	origin(tr[4],'陪我长大','段奥娟');
+}
+
+
+function choice(ys)  //设置checkbox的状态
+{
+	for(var i = 0; i<tr.length; i++)
+	{
+		tr[i].getElementsByTagName('td')[0].getElementsByTagName('input')[0].checked = ys;
+	}
+}
+
+function allchoice() //全选或全不选
+{
+	all.onclick = function()
+	{
+		choice(true);
+	}
+	nall.onclick = function()
+	{
+		choice(false);
+	}
+}
+
+function out() //删除功能
+{
+	var check = document.getElementsByClassName('checkbox');
+	delet.onclick = function()
+	{
+		for(var i = check.length-1; i >= 0; i--)
+		{
+			if(check[i].checked == true)
+			{
+				parent = tr[i].parentNode; //tr代表每个选项
+				parent.removeChild(parent.getElementsByTagName('tr')[i]);
+			}
+		}
+	}
+}
